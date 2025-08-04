86376682f8f4d74cb72675819f46a2dd569bdd16
PydahHasitha2420090107 <2420090107@klh.edu.in>
2025-08-04 15:21:05 +0530

    Add files via upload

---
+++ Basic_programs/src/basic_programs/Practical_1.java
@@ -0,0 +1,10 @@
+package basic_programs;
+
+public class Practical_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Basic_programs/src/basic_programs/Print_msg.java
@@ -0,0 +1,34 @@
+package basic_programs;
+import java.util.*;
+public class Print_msg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*Scanner sc= new Scanner(System.in);
+		System.out.println("Enter the digit");
+		int a=sc.nextInt();
+		sc.close();
+		System.out.println("Enter the digit");
+		int a=sc.nextInt();
+		if(a%2==0) {
+			System.out.println(a+" is even");
+		}else {
+			System.out.println(a+" is odd" );
+		}
+		System.out.println("Enter the digit");
+		int a=sc.nextInt();
+		sc.close();
+		if(a>0) {
+			System.out.println(a+" is positive");
+		}else if(a==0){
+			System.out.println(a+" is zero");
+		}else {
+			System.out.println(a+" is negative");
+		}
+		*/
+		/*
+		for(int i=1;i<11;i++) {
+			System.out.println(i);
+		}*/
+	}
+}
